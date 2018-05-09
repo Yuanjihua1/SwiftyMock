bfd974337b38bad846910b8ad7acc7ee1f912c40
ZhangJing <>
2018-05-09 15:41:48 +0800

    reinit

--- Mock/Pods/Target Support Files/Pods-MockTests/Pods-MockTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MockTests : NSObject
-@end
-@implementation PodsDummy_Pods_MockTests
-@end
--- Mock/Pods/Target Support Files/Pods-MockUITests/Pods-MockUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MockUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MockUITests
-@end
